19d13649826a0063cc8226e6bfeab732d606d1b3
David Delassus <david.jose.delassus@gmail.com>
2026-01-31 21:03:50 +0000

    :sparkles: add authentication

--- sources/game/src/bot.c
+++ sources/game/src/bot.c
@@ -38,6 +38,53 @@ static RESULT(UNIT, str) bot__fallback(void *udata, irc_msg *msg) {
 }
 
 
+// MARK: - auth
+RESULT(UNIT, str) bot__auth(bot *self) {
+  assert(self != NULL);
+
+  allocator a = std_allocator();
+
+  irc_msg m_nick     = {};
+  m_nick.has_prefix  = false;
+  m_nick.command     = str_literal("NICK");
+  m_nick.param_count = 1;
+  m_nick.params[0]   = self->nick;
+
+  str s_nick = irc_msg_encode(&m_nick, a);
+  RESULT(UNIT, conn_error) res = conn_write(self->conn, s_nick);
+  str_free(a, &s_nick);
+  if (!res.is_ok) {
+    return (RESULT(UNIT, str)){
+      .is_ok = false,
+      .err   = strview_from_cstr(conn_strerror(res.err)),
+    };
+  }
+
+  irc_msg m_user     = {};
+  m_user.has_prefix  = false;
+  m_user.command     = str_literal("USER");
+  m_user.param_count = 3;
+  m_user.params[0]   = self->nick;
+  m_user.params[1]   = str_literal("0");
+  m_user.params[2]   = str_literal("*");
+  m_user.trailing    = str_literal("mudirc bot");
+
+  str s_user = irc_msg_encode(&m_user, a);
+  res = conn_write(self->conn, s_user);
+  str_free(a, &s_user);
+  if (!res.is_ok) {
+    return (RESULT(UNIT, str)){
+      .is_ok = false,
+      .err   = strview_from_cstr(conn_strerror(res.err)),
+    };
+  }
+
+  return (RESULT(UNIT, str)){
+    .is_ok = true,
+  };
+}
+
+
 // MARK: - api
 RESULT(UNIT, str) bot_init(bot *self, const char *dbpath, conn_ref c, str nick) {
   assert(self   != NULL);
@@ -57,9 +104,7 @@ RESULT(UNIT, str) bot_init(bot *self, const char *dbpath, conn_ref c, str nick) 
   self->conn = c;
   self->nick = nick;
 
-  return (RESULT(UNIT, str)){
-    .is_ok = true,
-  };
+  return bot__auth(self);
 }
 
 
